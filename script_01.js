ecd04c12636c6b10af2b1514652acfeeddae09c2
Peter Weber <peter-aalen@gmx.net>
2023-04-17 12:47:47 +0200

    Baikal

--- script_01.js
+++ script_01.js
@@ -71,9 +71,24 @@
 // deep - deeper - deepest
 // nested object {{{}}}
 
+let baikal = {
+                value:"10m",
+                deep:{
+                        deeper:{
+                                    deepest:"Das Licht - auf 1642m!",
+                                    temperature: "4 Grad Celsius"
+                        }
+                }
 
 
 
+            };
+
+
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
+output(baikal.deep.deeper.temperature);
 
 
 /** Ausgabe */
